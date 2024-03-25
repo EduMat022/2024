1e0333e427b40757060598eabc4b2c6f743356aa
EduMat022 <111879691+EduMat022@users.noreply.github.com>
2024-03-25 11:01:22 -0300

    Add files via upload

---
+++ Eduardo 3°B/main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao")
+const textos = document.querySelectorAll(".aba-conteudo")
+
+for(leti=0;i <botoes.length;i=++){
+    botoes.onclick = function() {
+        for(let j=0; j<botoes.length;j++){
+            
+        }
+    }
+}
