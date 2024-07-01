1bdd26f033045e41759ddf84941d66c0764f6b1c
EduMat022 <111879691+EduMat022@users.noreply.github.com>
2024-07-01 11:02:59 -0300

    Add files via upload

---
+++ I.A/main.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaperguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const caixaResultao1 = document.querySelector(".caixa-principal")
+
+const perguntas = [
+    {
+        enunciado: "O esporte ajuda na sustentabilidade de tradições?",
+        alternativas: [
+            "sim."
+            "não."
+        ]
+    },
+]
