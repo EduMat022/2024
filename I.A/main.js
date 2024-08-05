ead174492256d8c428ebbf449680929bb5bdf888
EduMat022 <111879691+EduMat022@users.noreply.github.com>
2024-08-05 08:52:42 -0300

    Add files via upload

--- I.A/main.js
+++ I.A/main.js
@@ -1,17 +1,36 @@
-const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaperguntas = document.querySelector(".caixa-perguntas")
-const caixaAlternativas = document.querySelector(".caixa-alternativas")
-const caixaResultado = document.querySelector(".caixa-resultado")
-const textoResultado = document.querySelector(".texto-resultado")
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const caixaResultao1 = document.querySelector(".caixa-principal")
+const caixaResultado1 = document.querySelector(".caixa-principal");
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
-const perguntas = [
-    {
-        enunciado: "O esporte ajuda na sustentabilidade de tradições?",
-        alternativas: [
-            "sim."
-            "não."
-        ]
-    },
-]
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciados;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+    
+    const perguntas = [
+        {
+            enunciado: "O esporte ajuda na sustentabilidade de tradições?",
+            alternativas: [
+                {
+                    texto: "Isso é assustador!",
+                    afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                },
+                {
+                    texto: "Isso é maravilhoso!",
+                    afirmacao: "Quis saber como usar IA no seu dia a dia."
+                }
+            ]
+        }
